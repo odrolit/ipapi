9d4f4a15faef0588e44c3db6c995838f2b3789d9
root <odrolit@gmail.com>
2021-02-18 12:24:09 +0100

    user redefined

--- ipapi/user.py
+++ ipapi/user.py
@@ -5,45 +5,24 @@ from .base import *
 class user(base):
   """
   user class
+  
+  each user has access defined in list
+    data['parents']['access']
+  
+  each item has access defined in lists
+    data['access']['get']
+    data['access']['post']
+    data['access']['patch']
+    data['access']['put']
+    data['access']['delete']
   """
   
-  IMPLICIT_PARENTS = True
-  
-  def __init__(self, data, data_source = 'request'):
-    '''
-    puts data to self.data and sets object properties
-    
-    data_source can be request or database
-    '''
-    super().__init__(data, data_source)
-  
-  def _get_parents(self):
-    '''
-    returns list containing direct parents or root document
-    TODO
-    '''
-    return g.user.find({'_meta._valid': True,
-                     'scope': self.data['scope'],
-                     '_bin._first': {'$lte': self.data['_bin']['_first']},
-                     '_bin._last': {'$gte': self.data['_bin']['_last']}}, {'name': 1, })
-  
-  def _already_exists(self):
-    '''
-    returns True if already exists
-    '''
-    if self._find_one({'name': self.data['name'],
-                       'prefix': self.data['prefix'],
-                       'scope': self.data['scope']}):
-      return True
-
 
 
 class UserView(MethodView):
   '''
-  API operations: get, post, put, delete
+  API operations: get, post, delete, put, patch
   '''
-  method_decorators = []
-  aaas = {}
   
   def get(self, **kwargs):
     try:
@@ -52,24 +31,28 @@ class UserView(MethodView):
       return e400(e)
   
   def post(self):
-    return user.post(request.json)
-
+    try:
+      return user.post(request.json)
+    except Exception as e:
+      return e400(e)
   
-  def put(self, id):
-    id = int(id)
-    if id in self.aaas:
-      aaa = self.aaas[id]
-    else:
-      aaa = {'id': id}
-      self.aaas[id] = aaa
-    aaa['name']  = request.json.get('name'),
-    aaa['tag']  = request.json.get('tag'),
-    aaa['time']  = datetime.now()
-    return aaa, 201
+  def patch(self):
+    try:
+      return user.patch(request.json)
+    except Exception as e:
+      return e400(e)
   
-  def delete(self, _id):
-    return user.delete(_id, request.json)
-
+  def delete(self):
+    try:
+      return user.delete(request.json)
+    except Exception as e:
+      return e400(e)
+  
+  def put(self):
+    try:
+      return user.put(request.json)
+    except Exception as e:
+      return e400(e)
   
   def search(self, **kwargs):
     return user.get(**kwargs)
