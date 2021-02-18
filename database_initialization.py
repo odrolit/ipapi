b07af6ed60493e1167d85a9f0a91ec7eaca19b5b
root <odrolit@gmail.com>
2021-02-18 12:18:42 +0100

    access modified

--- database_initialization.py
+++ database_initialization.py
@@ -21,6 +21,7 @@ are deleted, use with caution!
 import argparse
 from pymongo import MongoClient, TEXT
 from flask import g
+from uuid import uuid4
 import ipapi
 
 
@@ -37,11 +38,13 @@ parser.add_argument('--force_delete_all_data',
 args = parser.parse_args()
 
 
+
 with app.app.app_context():
   ipapi.log.w('please check if LOGLEVEL is DEBUG')
   #create text indexes and root documents for all collections
   g._user_name = g._provider_name = 'root'
   g._user_ip = g._provider_ip = '127.0.0.1'
+  g.rid = uuid4()
   collections = [i for i in ipapi.base.collections()]
   ipapi.log.d(f'Collections: {collections}')
   for i in collections:
@@ -57,7 +60,8 @@ with app.app.app_context():
       data = {}
       data['description'] = f'Root {i} document'
       if i == 'ipv4':
-        db[i].create_index([('scope', 1), 
+        db[i].create_index([('_meta._active', 1),
+                            ('scope', 1), 
                             ('_bin._first', 1), 
                             ('_bin._last', 1)])
         data['name'] = '0.0.0.0'
@@ -67,7 +71,7 @@ with app.app.app_context():
         ii.data['_meta']['_version'] = 1
         ii.data['_meta']['_uuid_active'] = ii.data['_id']
         ii.data['parents'] = {}
-        ii.data['parents']['ipv4'] = [ii.data['_id']]
+        ii.data['parents']['ipv4'] = []
         root_ipv4 = db.ipv4.insert_one(ii.data).inserted_id
         ipapi.log.i(f'Created document {ii.data}')
       elif i == 'user':
@@ -76,86 +80,38 @@ with app.app.app_context():
         ii.data['_meta']['_version'] = 1
         ii.data['_meta']['_uuid_active'] = ii.data['_id']
         ii.data['parents'] = {}
-        ii.data['parents']['user'] = [ii.data['_id']]
+        ii.data['parents']['user'] = []
         root_user = db.user.insert_one(ii.data).inserted_id
         ipapi.log.i(f'Created document {ii.data}')
-      elif i == 'group':
+      elif i == 'access':
         data['name'] = 'root'
-        ii = ipapi.group(data, 'database_initialization')
+        ii = ipapi.access(data, 'database_initialization')
         ii.data['_meta']['_version'] = 1
         ii.data['_meta']['_uuid_active'] = ii.data['_id']
         ii.data['parents'] = {}
-        ii.data['parents']['group'] = [ii.data['_id']]
-        root_group = db.group.insert_one(ii.data).inserted_id
-        ipapi.log.i(f'Created document {ii.data}')
-        
-        data['name'] = 'root_delete'
-        ii = ipapi.group(data, 'database_initialization')
-        ii.data['_meta']['_version'] = 1
-        ii.data['_meta']['_uuid_active'] = ii.data['_id']
-        ii.data['parents'] = {}
-        ii.data['parents']['group'] = [root_group]
-        root_delete_group = db.group.insert_one(ii.data).inserted_id
-        ipapi.log.i(f'Created document {ii.data}')
-        
-        data['name'] = 'root_get'
-        ii = ipapi.group(data, 'database_initialization')
-        ii.data['_meta']['_version'] = 1
-        ii.data['_meta']['_uuid_active'] = ii.data['_id']
-        ii.data['parents'] = {}
-        ii.data['parents']['group'] = [root_group]
-        root_get_group = db.group.insert_one(ii.data).inserted_id
-        ipapi.log.i(f'Created document {ii.data}')
-        
-        data['name'] = 'root_patch'
-        ii = ipapi.group(data, 'database_initialization')
-        ii.data['_meta']['_version'] = 1
-        ii.data['_meta']['_uuid_active'] = ii.data['_id']
-        ii.data['parents'] = {}
-        ii.data['parents']['group'] = [root_group]
-        root_patch_group = db.group.insert_one(ii.data).inserted_id
-        ipapi.log.i(f'Created document {ii.data}')
-        
-        data['name'] = 'root_post'
-        ii = ipapi.group(data, 'database_initialization')
-        ii.data['_meta']['_version'] = 1
-        ii.data['_meta']['_uuid_active'] = ii.data['_id']
-        ii.data['parents'] = {}
-        ii.data['parents']['group'] = [root_group]
-        root_post_group = db.group.insert_one(ii.data).inserted_id
-        ipapi.log.i(f'Created document {ii.data}')
-        
-        data['name'] = 'root_put'
-        ii = ipapi.group(data, 'database_initialization')
-        ii.data['_meta']['_version'] = 1
-        ii.data['_meta']['_uuid_active'] = ii.data['_id']
-        ii.data['parents'] = {}
-        ii.data['parents']['group'] = [root_group]
-        root_put_group = db.group.insert_one(ii.data).inserted_id
+        ii.data['parents']['access'] = []
+        root_access = db.access.insert_one(ii.data).inserted_id
         ipapi.log.i(f'Created document {ii.data}')
       else:
         ipapi.log.e(f'Unhandled {i}')
+        raise Exception(f'Unhandled {i}')
     else:
       ipapi.log.w(f'Skipping initialization, {i} is not empty!')
     ipapi.log.i(f'Listing indexes for {i}')
     for j in db[i].list_indexes():
       ipapi.log.i(f'\t{j}')
   if args.force_delete_all_data:
-    #assing user root to parent groups
-    r = db.user.update_one({'_id': root_user}, {'$set': {'parents': {'group': [
-      root_delete_group,
-      root_get_group,
-      root_patch_group,
-      root_post_group,
-      root_put_group]}}}).modified_count
-    ipapi.log.i(f'{r} user added to root group')
+    #assign user root to parent accesses
+    r = db.user.update_one({'_id': root_user},
+      {'$set': {'parents': {'access': [root_access]}}})
+    ipapi.log.i(f'{r.modified_count} user added to root access')
     #create access for root documents
     root_access =  {
-      'delete': [root_delete_group],
-      'get': [root_get_group],
-      'patch': [{'match': '.', 'patch': [root_patch_group]}],
-      'post': [root_post_group],
-      'put': [root_put_group]}
+      'delete': [root_access],
+      'get': [root_access],
+      'patch': [{'match': '.', 'patch': [root_access]}],
+      'post': [root_access],
+      'put': [root_access]}
     for i in collections:
       #update all
       if i == 'ipv4':
@@ -164,8 +120,9 @@ with app.app.app_context():
       elif i == 'user':
         r = db.user.update_many({}, {'$set': {'access': root_access}})
         ipapi.log.i(f'Access added to {r.modified_count} root {i} documents')
-      elif i == 'group':
-        r = db.group.update_many({}, {'$set': {'access': root_access}})
+      elif i == 'access':
+        r = db.access.update_many({}, {'$set': {'access': root_access}})
         ipapi.log.i(f'Access added to {r.modified_count} root {i} documents')
       else:
         ipapi.log.e(f'Unhandled {i}')
+        raise Exception(f'Unhandled {i}')
