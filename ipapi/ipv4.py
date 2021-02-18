b39d0cb1e4937de04e4abe066d54c5ccf6f8fd03
root <odrolit@gmail.com>
2021-02-18 12:22:22 +0100

    get_active_valid_parents now uses class function

--- ipapi/ipv4.py
+++ ipapi/ipv4.py
@@ -17,6 +17,7 @@ class ipv4(base):
     '''
     super().__init__(data, data_source)
     
+    log.d((rid(), now(), '__init__', self._class, self.data_source, data))
     n = IPv4Network(f'{self.data["name"]}/{self.data["prefix"]}')
     #check request
     if self.data_source == 'request':
@@ -28,58 +29,65 @@ class ipv4(base):
       self.data['_bin'] = {}
       self.data['_bin']['_first'] = n.network_address.packed
       self.data['_bin']['_last'] = n.broadcast_address.packed
+    log.d((rid(), now(), '__init__', self._class, self.data_source, data))
   
-  def _C_M_get_active(self):
+  def _C_U_get_active(self):
     '''
     returns active document or None,
     the document can be valid or deleted
     '''
-    log.d(('_C_M_get_active'))
     r = self._find_one({'_meta._active': True,
-                        'name': self.data['name'],
-                        'prefix': self.data['prefix'],
-                        'scope': self.data['scope']})
+                        'scope': self.data['scope'],
+                        '_bin._first': self.data['_bin']['_first'],
+                        '_bin._last': self.data['_bin']['_last']})
+    log.d((rid(), now(), '_C_U_get_active', r))
     return r
   
-  def _C_M_get_active_valid_parents(self):
+  def _C_U_get_active_valid_parents(self):
     '''
     returns list containing direct active
     and valid parents or root document
     '''
-    log.d(('_C_M_get_active_valid_parents'))
-    r = g.ipv4.find({'_meta._active': True,
+    r = self._find_multi_simple({'_meta._active': True,
                      '_meta._valid': True,
                      'scope': self.data['scope'],
                      '_bin._first': {'$lte': self.data['_bin']['_first']},
-                     '_bin._last': {'$gte': self.data['_bin']['_last']}}, {'name': 1, })
-    return r.sort('prefix', ASCENDING).limit(1)
+                     '_bin._last': {'$gte': self.data['_bin']['_last']}})
+    r.sort('prefix', ASCENDING).limit(1)
+    log.d((rid(), now(), '_C_U_get_active_valid_parents', r.count()))
+    return r
   
   def _I_P_is_leaf(self):
     '''
     returns True if self is leaf
     '''
-    log.d(('_I_P_is_leaf'))
     if self.data['prefix'] == 32:
-      return True
+      r = True
+    else:
+      r = False
+    log.d((rid(), now(), '_I_P_is_leaf', r))
+    return r
   
   def _I_P_is_parent_of(self, child):
     '''
     returns True if self is parent of child
     '''
-    log.d(('_I_P_is_parent_of'))
-    return self.data['scope'] == child['scope'] and \
+    r = self.data['scope'] == child['scope'] and \
       self.data['prefix'] < child['prefix'] and \
       self.data['_bin']['_first'] <= child['_bin']['_first'] and \
       self.data['_bin']['_last'] >= child['_bin']['_last']
+    log.d((rid(), now(), '_I_P_is_parent_of', r))
+    return r
   
   def _I_P_is_equal_to(self, second):
     '''
     returns True if self equals to second
     '''
-    log.d(('_I_P_is_equal_to'))
-    return self.data['scope'] == second['scope'] and \
+    r = self.data['scope'] == second['scope'] and \
       self.data['name'] == second['name'] and \
       self.data['prefix'] == second['prefix']
+    log.d((rid(), now(), '_I_P_is_equal_to', r))
+    return r
   
   
   def _C_O_parent_added(self, parent):
@@ -88,25 +96,33 @@ class ipv4(base):
     
     expects parent instance as parameter
     '''
-    log.d(('_C_O_parent_added', self._class, self.data['_id'],
+    log.d((rid(), now(), '_C_O_parent_added', self._class, self.data['_id'],
            parent._class, parent.data['_id']))
-    if parent._class == 'ipv4_whitelist':
-      #blackholed IP can not be whitelisted
-      if any(i in self.data['parents'] for i in ('ipv4_blackhole',
-                                                 'ipv4_blackhole_add', 'ipv4_blackhole_del')):
-        raise Exception(f'can not add {self.data[_id]} to whitelist, is blackholed')
-    elif parent._class in ('ipv4_blackhole',
-                           'ipv4_blackhole_add',
-                           'ipv4_blackhole_del'):
-      #whitelisted IP can not be blackholed
-      if self.data['parents']['ipv4_whitelist']:
-        raise Exception(f'can not add {self.data[_id]} to blackhole, is whitelisted')
-      #only global scope can be blackholed
+    if parent._class == 'ipv4_protected':
+      #nexthop IP can not be protected
+      if any(i in self.data['parents'] for i in ('ipv4_nexthop',
+                                                 'ipv4_nexthop_add', 'ipv4_nexthop_del')):
+        log.e((rid(), now(), '_C_O_parent_added', self._class, self.data['_id'],
+               parent._class, parent.data['_id']))
+        raise Exception(f'can not add {self.data[_id]} to protected, is nexthop')
+    elif parent._class in ('ipv4_nexthop',
+                           'ipv4_nexthop_add',
+                           'ipv4_nexthop_del'):
+      #protected IP can not have nexthop
+      if self.data['parents']['ipv4_protected']:
+        log.e((rid(), now(), '_C_O_parent_added', self._class, self.data['_id'],
+               parent._class, parent.data['_id']))
+        raise Exception(f'can not add {self.data[_id]} to nexthop, is protected')
+      #nexthop can be set only for global scope
       if self.data['scope'] != 'global':
-        raise Exception(f'can not add {self.data[_id]} to blackhole, not global')
-    #TODO move to ipv4_blackhole._C_O_child_added()
-    if parent._class == 'ipv4_blackhole':
-      ipv4_blackhole_add(parent_id, self.data['name'], self.data['prefix'])
+        log.e((rid(), now(), '_C_O_parent_added', self._class, self.data['_id'],
+               parent._class, parent.data['_id']))
+        raise Exception(f'can not add {self.data[_id]} to nexthop, not global')
+    #TODO move to ipv4_nexthop._C_O_child_added()
+    if parent._class == 'ipv4_nexthop':
+      ipv4_nexthop_add(parent_id, self.data['name'], self.data['prefix'])
+    log.d((rid(), now(), '_C_O_parent_added', self._class, self.data['_id'],
+           parent._class, parent.data['_id']))
   
   def _C_O_parent_deleted(self, parent):
     '''
@@ -114,11 +130,13 @@ class ipv4(base):
         
     expects parent instance as parameter
     '''
-    log.d(('_C_O_parent_deleted', self._class, self.data['_id'],
+    log.d((rid(), now(), '_C_O_parent_deleted', self._class, self.data['_id'],
            parent._class, parent.data['_id']))
-    #TODO move to ipv4_blackhole._C_O_child_deleted()
-    if parent._class == 'ipv4_blackhole':
-      ipv4_blackhole_add(parent_id, self.data['name'], self.data['prefix'])
+    #TODO move to ipv4_nexthop._C_O_child_deleted()
+    if parent._class == 'ipv4_nexthop':
+      ipv4_nexthop_add(parent_id, self.data['name'], self.data['prefix'])
+    log.d((rid(), now(), '_C_O_parent_deleted', self._class, self.data['_id'],
+           parent._class, parent.data['_id']))
 
 
 
@@ -126,8 +144,6 @@ class Ipv4View(MethodView):
   '''
   API operations: get, post, delete, put, patch
   '''
-  method_decorators = []
-  aaas = {}
   
   def get(self, **kwargs):
     try:
@@ -147,27 +163,17 @@ class Ipv4View(MethodView):
     except Exception as e:
       return e400(e)
   
-  def delete(self, **kwargs):
+  def delete(self):
     try:
-      return ipv4.delete(**kwargs)
+      return ipv4.delete(request.json)
     except Exception as e:
       return e400(e)
   
-  def put(self, **kwargs):
+  def put(self):
     try:
-      return ipv4.put(**kwargs)
+      return ipv4.put(request.json)
     except Exception as e:
       return e400(e)
-    id = int(id)
-    if id in self.aaas:
-      aaa = self.aaas[id]
-    else:
-      aaa = {'id': id}
-      self.aaas[id] = aaa
-    aaa['name']  = request.json.get('name'),
-    aaa['tag']  = request.json.get('tag'),
-    aaa['time']  = datetime.now()
-    return aaa, 201
   
   def search(self, **kwargs):
     return ipv4.get(**kwargs)
